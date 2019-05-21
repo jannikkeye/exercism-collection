1a65e489fbfa11efc0656ded6730a439b51e5f85
Jannik Keye <jannik.keye@gmail.com>
2019-05-21 19:22:59 +0200

    fix(rust): clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,58 +1,54 @@
+#![feature(euclidean_division)]
+
 use std::fmt;
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq)]
 pub struct Clock {
-    hours: u8,
-    minutes: u8,
-}
-
-fn resolve_minutes(minutes: i32) -> (i32, i32) {
-    let mut resolved_minutes = minutes % 60;
-    let mut resolved_hours = minutes / 60;
-
-    if minutes > 0 && resolved_minutes == 0 {
-        resolved_minutes = 0;
-    } else if minutes < 0 && resolved_minutes != 0 {
-        resolved_hours -= 1;
-        resolved_minutes += 60;
-    }
-
-    (resolved_hours, resolved_minutes)
+    minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let (resolved_hours, resolved_minutes) = resolve_minutes(minutes);
-        let hours = (hours + resolved_hours) % 24;
-
-
-
         Clock {
-            hours: if hours < 0 { hours + 24 } else { hours } as u8,
-            minutes: if resolved_minutes < 0 { resolved_minutes + 60 } else { resolved_minutes } as u8,
+            minutes: minutes + hours * 60,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        if minutes == 0 {
-            return Self::new(self.hours as i32, self.minutes as i32);
-        }
+       Clock {
+           minutes: self.minutes + minutes,
+       }
+    }
+}
 
-        let (resolved_hours, resolved_minutes) = resolve_minutes(minutes + self.minutes as i32);
-
-        let hours = (self.hours as i32 + resolved_hours) % 24;
-
-        Self::new(hours, resolved_minutes)
+impl PartialEq for Clock {
+    fn eq(&self, other: &Clock) -> bool {
+        self.to_string() == other.to_string()
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut hours = (self.minutes / 60).rem_euclid(24);
+        let mut minutes = self.minutes % 60;
+
+        if minutes < 0 { 
+            hours -= 1;
+
+            if hours < 0 {
+                hours = 24 + hours;
+            } 
+        }
+
+        if minutes < 0 {
+            minutes = 60 + minutes;
+        }
+
         write!(
             f,
-            "{}:{}",
-            if self.hours < 10 { format!("0{}", self.hours) } else { self.hours.to_string() },
-            if self.minutes < 10 { format!("0{}", self.minutes) } else { self.minutes.to_string() },
+            "{:02}:{:02}",
+            hours,
+            minutes
         )
     }
 }
